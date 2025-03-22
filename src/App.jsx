880cd8f09d5a023d1525a68033f1b43d44a815a0
MohamedSameer007 <230220.it@rmkec.ac.in>
2025-03-22 09:51:00 +0530

    new file added

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState("")
+  
+  const handleClick = (value) => {
+    setInput((prev) => prev + value)
+  }
+
+  const handleClear = () => {
+    setInput("")
+  }
+
+  const handleResult = () => {
+    try{
+      setInput(eval(input).toString())
+    }
+    catch(error){
+      setInput("error")
+    }
+  }
+  return (
+    <>
+    <div className='container'>
+      <h1>Calculator <i class="fa-solid fa-calculator"></i></h1>
+
+      <input value={input}></input>
+
+      <div className='buttons'>
+
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button className='symbol' onClick={() => handleClick("/")}>/</button>
+
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button className='symbol' onClick={() => handleClick("*")}>*</button>
+
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button className='symbol' onClick={() => handleClick("-")}>-</button>
+
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button className='calc' onClick={handleClear}>c</button>
+        <button className='symbol' onClick={() => handleClick("+")}>+</button>
+
+        <button className='equal' onClick={handleResult}>=</button>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+
+export default App
